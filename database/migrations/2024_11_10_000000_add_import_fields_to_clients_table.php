64d516c0f913888dadfd7156ab63ae01dda55a6f
Tinashe P Chaitezvi <tinashepresleychaitezvi00@gmail.com>
2024-11-27 14:42:49 +0200

    wip

--- database/migrations/2024_11_10_000000_add_import_fields_to_clients_table.php
+++ database/migrations/2024_11_10_000000_add_import_fields_to_clients_table.php
@@ -14,16 +14,6 @@ return new class extends Migration
         Schema::table('clients', function (Blueprint $table) {
             $table->string('customer_id')->nullable()->after('id');
             $table->string('phone')->nullable()->after('mobile');
-            $table->string('trading_name')->nullable()->after('name');
-            $table->unsignedBigInteger('registration_country_id')->nullable()->after('country_id');
-            $table->unsignedBigInteger('main_bank_id')->nullable()->after('registration_country_id');
-            $table->unsignedBigInteger('second_bank_id')->nullable()->after('main_bank_id');
-            $table->unsignedBigInteger('third_bank_id')->nullable()->after('second_bank_id');
-            
-            $table->foreign('registration_country_id')->references('id')->on('countries')->onDelete('set null');
-            $table->foreign('main_bank_id')->references('id')->on('banks')->onDelete('set null');
-            $table->foreign('second_bank_id')->references('id')->on('banks')->onDelete('set null');
-            $table->foreign('third_bank_id')->references('id')->on('banks')->onDelete('set null');
         });
     }
 
@@ -33,19 +23,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('clients', function (Blueprint $table) {
-            $table->dropForeign(['registration_country_id']);
-            $table->dropForeign(['main_bank_id']);
-            $table->dropForeign(['second_bank_id']);
-            $table->dropForeign(['third_bank_id']);
-            
             $table->dropColumn([
                 'customer_id',
-                'phone',
-                'trading_name',
-                'registration_country_id',
-                'main_bank_id',
-                'second_bank_id',
-                'third_bank_id'
+                'phone'
             ]);
         });
     }
